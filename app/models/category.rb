b92199f5cd1501d0fb4d53a9d83f3ece3c9d6ab5
Ryan <rrowe@andrew.cmu.edu>
2012-05-01 20:11:49 -0400

    article created with relationships and scops

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :photos
+  has_many :article
   
   scope :active, where('active = ?', true)
-  scope :alphabetical, order('name')
+  scope :alphabetical, order('title')
 end
